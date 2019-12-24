0784cda594c6043a4314849e7a6c351043a0e67f
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2019-12-24 21:45:31 +0900

    :trollface: functionsがぐちゃった

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,14 +1,22 @@
-set -q FZF_TMUX_HEIGHT; or set -U FZF_TMUX_HEIGHT "40%"
-set -q FZF_DEFAULT_OPTS; or set -U FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
-set -q FZF_LEGACY_KEYBINDINGS; or set -U FZF_LEGACY_KEYBINDINGS 1
-set -q FZF_PREVIEW_FILE_CMD; or set -U FZF_PREVIEW_FILE_CMD "head -n 10"
-set -q FZF_PREVIEW_DIR_CMD; or set -U FZF_PREVIEW_DIR_CMD "ls"
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+set -x FZF_DEFAULT_COMMAND      'rg --files --hidden --follow --glob "!.git/*" 2> /dev/null'
+set -x FZF_DEFAULT_OPTS         '--height 80% --reverse --border'
+
+set -x FZF_FIND_FILE_COMMAND    'find . -type d -name '.git' -prune -o -type f -print  2> /dev/null'
+set -x FZF_FIND_FILE_OPTS       '--height 80% --reverse --border --preview "coderay {} | head -n 30"'
+
+set -x FZF_CD_COMMAND           'find . -type d -name '.git' -prune -o -type d -print 2> /dev/null'
+set -x FZF_CD_OPTS              '--height 80% --reverse --border --preview "tree -C {} | head -200"'
+
+set -x FZF_TMUX                 1
+set -x FZF_TMUX_HEIGHT          70%
 
 function fzf_uninstall -e fzf_uninstall
-    # disabled until we figure out a sensible way to ensure user overrides
-    # are not erased
-    # set -l _vars (set | command grep -E "^FZF.*\$" | command awk '{print $1;}')
-    # for var in $_vars
-    #     eval (set -e $var)
-    # end
+  # disabled until we figure out a sensible way to ensure user overrides
+  # are not erased
+  # set -l _vars (set | command grep -E "^FZF.*\$" | command awk '{print $1;}')
+  # for var in $_vars
+  #     eval (set -e $var)
+  # end
 end
