55866837da804bec38c288154ba88064d7651cd8
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2021-02-09 17:11:20 +0900

    :tada: どっかん

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ set -x GOPATH /home/fill/src/go
 
 set -x CHROME_EXECUTABLE '/usr/bin/google-chrome-stable'
 
-set PATH /home/fill/src/go/bin /home/fill/flutter/bin /opt/dart-sdk /home/fill/src/dotfiles/bin/shell /home/fill/.rbenv/versions/2.7.1/bin ~/local/matlab/bin $PATH
+set PATH /home/fill/src/go/bin /home/fill/flutter/bin /opt/dart-sdk /home/fill/src/dotfiles/bin/shell /home/fill/.rbenv/versions/2.7.1/bin ~/local/matlab/bin ~/src/github.com/fillu87gyc/data-science/libsvm-3.24 $PATH
 
 function fish_right_prompt; end
 
@@ -36,6 +36,7 @@ alias sys='sudo systemctl'
 alias smb_start='sudo smb_start.sh'
 
 alias gl='git log --color --graph --pretty=format:"%C(red reverse)%d%Creset%C(white reverse) %h% Creset %C(green reverse) %an<@%cn> %Creset %C(cyan)%ar%Creset%n%C(white bold)%w(80) %s%Creset%n%n%w(80,2,2)%b" | emojify | less -r'
+alias dockerd='docker build --pull --rm -f "Dockerfile" -t yuukikaede/pkg-buildin:(LC_ALL=C date "+%Y-%h%d-%H%M%S") "."'
 
 function g --wraps git
   hub $argv;
